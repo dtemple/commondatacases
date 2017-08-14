df0da5537d8df5e6194c117e2dd350547f790051
dtemple <dtemple@gmail.com>
2017-08-14 12:51:08 -0700

    selecting and acting on dfs

--- data_wrangling.py
+++ data_wrangling.py
@@ -44,5 +44,40 @@ train_df=train_df.drop('Cabin', axis=1)
 
 ####### Using Pivot Tables and Group By #########
 
-# Pivot just a subset of the data (note the as_index defaults to true) #
+## Use Group By on just a subset of the data (note the as_index defaults to true) ##
 x=train_df[['Sex','Survived']].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived',ascending=False)
+
+# Group by and then apply a function #
+y=train_df[['Sex','SibSp','Parch']].groupby('Sex').sum()
+
+# Heirarchical indexing in a Group by #
+z=train_df[['Pclass','Sex','SibSp','Parch']].groupby(['Pclass','Sex']).sum()
+
+## Pivot tables ##
+# Pivot table a single value by count/mean/etc #
+x=pd.pivot_table(train_df, values=['SibSp'],index=['Sex'], aggfunc='count')
+
+# Pivot a set of columns #
+x=pd.pivot_table(train_df[['Pclass','Sex','SibSp','Parch']], index=['Sex'], aggfunc='mean')
+
+# Pivot a set of columns and break it down by the values of one column #
+x=pd.pivot_table(train_df[['Pclass','Sex','SibSp','Parch']], index=['Sex'], aggfunc='mean', columns='Pclass')
+
+#           SibSp                         Parch
+#Pclass         1         2         3         1         2         3
+#Sex
+#female  0.553191  0.486842  0.895833  0.457447  0.605263  0.798611
+#male    0.311475  0.342593  0.498559  0.278689  0.222222  0.224784
+
+
+## Performing operations on dfs and pivots ##
+
+# Add a calculated column to a pivot table #
+x['sum']=x.sum(axis=1)
+
+# get the % who survived #
+x=train_df[['Survived','Embarked']].groupby(['Embarked'], as_index=False).sum() # The number of survivors
+y=train_df[['Survived','Embarked']].groupby(['Embarked'], as_index=False).count() # The number of total people
+y.rename(columns={'Survived':'total'}, inplace=True)
+
+z=pd.merge(x,y)
